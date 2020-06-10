18b72afda06e73549f4240841d8f73ea2b938873
patrick <patricksinclair@hotmail.co.uk>
2020-06-10 17:30:54 +0100

    now with method actually called

--- src/MultispeciesMain.java
+++ src/MultispeciesMain.java
@@ -2,5 +2,7 @@ public class MultispeciesMain {
     public static void main(String[] args) {
 
         int nReps = Integer.parseInt(args[0]);
+        double tau_val = 0.2;
+        BioSystem.varyingDeteriorationAndThreshold(nReps, tau_val);
     }
 }
